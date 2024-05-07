6a6dfefc46c720b2c92e42650edc0bf2a9a0f670
Krihag <125972171+Krihag@users.noreply.github.com>
2024-05-07 13:58:39 +0200

    style changes navbar loggedout
    added icon width and changed text-size between listing and py on desktop version.

--- src/js/components/header/loggedOut/index.js
+++ src/js/components/header/loggedOut/index.js
@@ -5,29 +5,29 @@ import registerModal from "../../modal/templates/register/index.js";
 export default function loggedOut(container) {
   const homeContainer = createEle(
     "a",
-    "flex text-lg items-center gap-5 lg:gap-12 p-1 lg:hidden",
+    "flex items-center gap-4 lg:gap-12 p-1 lg:hidden",
   );
-  const homeIcon = createEle("i", "fas fa-home lg:hidden");
-  const home = createEle("span", false, "Home");
+  const homeIcon = createEle("i", "fas fa-home lg:hidden w-4");
+  const home = createEle("span", null, "Home");
   homeContainer.href = "/";
   homeContainer.append(homeIcon, home);
 
   const loginContainer = createEle(
     "a",
-    "flex items-center text-lg gap-4 p-1 cursor-pointer lg:px-8  lg:mt-0 lg:order-5 lg:bg-cta lg:text-primary lg:rounded-full",
+    "flex items-center  gap-4 p-1 cursor-pointer lg:px-10 lg:py-2  lg:mt-0 lg:order-5 lg:bg-cta lg:text-primary lg:rounded-full",
   );
 
-  const loginIcon = createEle("i", "fas fa-sign-in-alt lg:hidden");
+  const loginIcon = createEle("i", "fas fa-sign-in-alt lg:hidden w-4");
   const login = createEle("span", false, "Login");
 
   loginContainer.append(loginIcon, login);
 
   const registerContainer = createEle(
     "a",
-    "flex items-center text-lg gap-4 p-1 cursor-pointer lg:px-8  lg:mt-0 lg:order-5 lg:border lg:rounded-full lg:border-purple-100",
+    "flex items-center  gap-4 p-1 cursor-pointer lg:px-8 lg:py-2 lg:mt-0 lg:order-5 lg:border lg:rounded-full lg:border-purple-100",
   );
 
-  const registerIcon = createEle("i", "fas fa-user-plus lg:hidden");
+  const registerIcon = createEle("i", "fas fa-user-plus lg:hidden w-4");
   const register = createEle("span", null, "Register");
 
   registerContainer.append(registerIcon, register);
