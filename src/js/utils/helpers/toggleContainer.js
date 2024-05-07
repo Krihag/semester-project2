408a741f47dca8a6969a249cff87de884284b29d
Krihag <125972171+Krihag@users.noreply.github.com>
2024-05-07 12:48:36 +0200

    updated JS docs and changed default value of clickInside to true

--- src/js/utils/helpers/toggleContainer.js
+++ src/js/utils/helpers/toggleContainer.js
@@ -1,15 +1,16 @@
 /**
  * Toggles the visibility of a container element when the provided element is clicked.
+ * If clicked anywhere outside of the container while its open, the container will close.
  * @param {HTMLElement} ele - The element that triggers the container visibility toggle.
  * @param {HTMLElement} container - The container element whose visibility will be toggled.
  * @param {string} [display="block"] - The display value to be set when the container is visible.
- * @param {boolean} [clickInside=false] - Set to false by default (container closes if clicked inside it or anywhere else on the page).
- * If changed to true clicking inside container will  not close it anymore (will stil close if clicked outside of this).
+ * @param {boolean} [clickInside=true] - Whether or not you should be able to click inside the container without closing it.
+ * (clickInside can be good to set to false if all options the container should close when clicked, and you dont want to handle them individually)
  */
 export default function (
   ele,
   container,
-  clickInside = false,
+  clickInside = true,
   display = "block",
 ) {
   function showContainer(e) {
