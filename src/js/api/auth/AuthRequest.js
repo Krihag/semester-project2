6df598f566ac0e69926b112a695b617e38e0a358
Krihag <125972171+Krihag@users.noreply.github.com>
2024-05-08 14:51:46 +0200

    removed console logs

--- src/js/api/auth/AuthRequest.js
+++ src/js/api/auth/AuthRequest.js
@@ -17,9 +17,6 @@ export default class AuthRequest {
     this.headers = headers;
     this.endpoint = endpoint;
     this.body = body;
-
-    console.log(this.endpoint);
-    console.log(this.body);
   }
 
   /**
@@ -31,7 +28,7 @@ export default class AuthRequest {
     if (!endpoint) throw new Error("Missing endpoint");
 
     const url = `${config.BASE_URL}/${endpoint}`;
-    console.log(url);
+
     try {
       const response = await fetch(url, {
         method: this.method,
