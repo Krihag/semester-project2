bdf0f0b1ceb95723feabec856633af08851495db
Krihag <125972171+Krihag@users.noreply.github.com>
2024-05-13 00:52:50 +0200

    added option on modal to close if clickedOutside (set to false by default)

--- src/js/components/modal/handler/toggleModal.js
+++ src/js/components/modal/handler/toggleModal.js
@@ -1,9 +1,11 @@
 const modalContainer = document.getElementById("modal-container");
 const modalHeader = document.getElementById("modal-header");
 const modalContent = document.getElementById("modal-content");
+const modalMain = document.getElementById("modal-main");
 
 /**
- * Object representing a modal toggle.
+ * @description
+ * This module is responsible for toggling the modal (open/close).
  * @namespace
  * @name modalToggle
  */
@@ -11,10 +13,19 @@ const modalToggle = {
   /**
    * Opens the modal.
    */
-  open: function () {
+  open: function (clickOutsideClose = false) {
     modalContainer.classList.remove("hidden");
     modalContainer.classList.add("flex");
     document.body.classList.add("overflow-hidden");
+
+    if (clickOutsideClose) {
+      const clickOutside = (e) => {
+        if (modalMain.contains(e.target)) return;
+        modalContainer.removeEventListener("click", clickOutside);
+        modalToggle.close();
+      };
+      modalContainer.addEventListener("click", clickOutside);
+    }
   },
   /**
    * Closes the modal.
